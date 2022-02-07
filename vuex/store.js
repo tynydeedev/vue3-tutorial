1cc85e3ffe62964f079ebb0dc1a66d5eb5b8347a
tynydeedev <tynydeedev@gmail.com>
2022-02-07 17:53:37 +0700

    section 7

---
+++ vuex/store.js
@@ -0,0 +1,45 @@
+import { createStore } from 'vuex';
+import { testPosts } from '../microblog/testPosts';
+
+const delay = () => new Promise((res) => setTimeout(res, 1000));
+
+const posts = {
+  namespaced: true,
+
+  state() {
+    return {
+      postId: null,
+      all: [],
+    };
+  },
+
+  mutations: {
+    setPostId(state, postId) {
+      state.postId = postId;
+    },
+    setPosts(state, posts) {
+      state.all = posts;
+    },
+  },
+
+  actions: {
+    async fetch(ctx, payload) {
+      await delay();
+      ctx.commit('setPosts', testPosts);
+    },
+  },
+
+  getters: {
+    currentPost(state) {
+      return state.all.find((x) => {
+        return x.id === state.postId;
+      });
+    },
+  },
+};
+
+export const store = createStore({
+  modules: {
+    posts,
+  },
+});
